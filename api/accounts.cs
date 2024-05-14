d6c345cdb1c6d28c95deaa361333140e57a0c342
IreneJai <122608089+IreneJai@users.noreply.github.com>
2024-05-14 10:42:44 +0800

    Add files via upload

---
+++ api/accounts.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2
+{
+    public class accounts
+    {
+        public string userName {  get; set; }
+
+        public string password { get; set; }
+
+        public string Name { get; set; }
+    }
+}
