206b1ddf6ed3996093bb69902b8726bebc041109
IreneJai <122608089+IreneJai@users.noreply.github.com>
2024-05-24 12:03:13 +0800

    DataBase_update

---
+++ api/AccountsDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication1
+{
+    public class AccountsDB : DbContext
+    {
+        public DbSet<accounts> Accounts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=AccountsDB.db");
+        }
+    }
+}
